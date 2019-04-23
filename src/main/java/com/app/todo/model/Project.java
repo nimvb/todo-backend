2f1224fc49041d6c3013f416f3cd553a3d946b95
Sina Rafati <sn.rafati@gmail.com>
2019-04-23 16:10:26 +0200

    new project developed

--- src/main/java/com/app/todo/model/Project.java
+++ src/main/java/com/app/todo/model/Project.java
@@ -1,9 +1,47 @@
 package com.app.todo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-
+@Table
+@Entity
 public class Project implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String name;
+    @OneToMany(mappedBy = "project", cascade= CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+
+    public Project() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
